15144a705abe83ed339de46311c3b8ca732e875c
Valdir Santos <valdirsantost40@gmail.com>
2024-07-04 16:26:50 -0300

    Teste 1 hamburguer

--- src/componentsPadrao/Header/Header.jsx
+++ src/componentsPadrao/Header/Header.jsx
@@ -5,6 +5,17 @@ export default function Header() {
     return (
         <header className="bg-cores-azul text-cores-branco">
             
+            <input type="checkbox" id="amostrar-lista-header" />
+            <label htmlFor="amostrar-lista-header">
+                <span className="bg-[url('/barras.svg')] flex w-[1.8rem] h-[1.5rem]"></span>
+            </label>
+            
+            <nav className="flex flex-col gap-[0.7rem] bg-cores-azul_medio" id="lista">
+                    <Link to={"/biblia/livros"}>Ler a Bíblia</Link>
+                    <Link to={"duvidas/tiraduvidas"}>Tirar dúvidas</Link>
+                    <a href="#vantagens">Vantagens</a>
+            </nav>
+
                 <div className="flex text-2xl lg: p-5 justify-between items-center text-center">
 
                 <div className=" flex m-auto">
